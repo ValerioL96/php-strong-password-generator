ec9059c3525c0e09f3a366eb5cd89cd7a40558d8
ValerioL96 <v.lembo1396@gmail.com>
2024-06-29 11:42:53 +0200

    created form login in file login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<form action="" method="GET">
+    <div>
+        <label for="username">
+            Username:
+        </label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div>
+        <label for="password">
+            Password:
+        </label>
+        <input type="password" name="password" id="password">
+    </div>
+</form>
